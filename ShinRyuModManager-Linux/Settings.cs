af2ac3421c70d92be8ad75ff9304f076d0b9ebf5
Benjamin <benjaminesims@gmail.com>
2025-08-23 11:01:38 -0500

    Cross-platform hidden flag files
    - Changed name of flag files to actually be hidden on all platforms

--- ShinRyuModManager-Linux/Settings.cs
+++ ShinRyuModManager-Linux/Settings.cs
@@ -7,13 +7,13 @@ public static class Settings {
     //UPDATES
     public const string UPDATER_EXECUTABLE_NAME = "RyuUpdater.exe";
     public const string UPDATE_FLAG_FILE_NAME = "update.txt";
-    public const string UPDATE_RECENT_FLAG_FILE_NAME = "SRMM_RECENT_UPDATE_FLAG";
+    public const string UPDATE_RECENT_FLAG_FILE_NAME = ".SRMM_RECENT_UPDATE_FLAG";
     public const string UPDATE_INFO_REPO_OWNER = "SRMM-Studio";
     public const string UPDATE_INFO_REPO = "srmm-version-info";
     public const string UPDATE_INFO_FILE_PATH = "RyuUpdater/config.yaml";
 
     //EVENTS
-    public const string EVENT_FOOLS24_FLAG_FILE_NAME = "SRMM_FOOLS24_FLAG";
+    public const string EVENT_FOOLS24_FLAG_FILE_NAME = ".SRMM_FOOLS24_FLAG";
     
     //LIBRARIES
     public const string LIBRARIES_INFO_REPO_OWNER = "SRMM-Studio";
